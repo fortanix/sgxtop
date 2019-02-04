47b437eea3234b01f509a9d214adfec877e59672
Kevin Lahey <kevin.lahey@fortanix.com>
2019-02-04 13:24:43 -0800

    Curses support, plus fix hash update

--- sgxtop.c
+++ sgxtop.c
@@ -35,12 +35,16 @@
  */
 
 #include <stdio.h>
+#include <curses.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
+#include <assert.h>
 #include <unistd.h>
 #include <errno.h>
 #include <time.h>
 #include <sys/queue.h>
+#include <linux/taskstats.h>
 
 #define SGX_STATS	"/proc/sgx_stats"
 #define SGX_ENCLAVES	"/proc/sgx_enclaves"
@@ -62,6 +66,7 @@ LIST_HEAD(enclave_head, enclave);
 struct enclave {
 	pid_t pid;
 	unsigned int id;
+	char *comm; /* command executing */
 	unsigned long size;
 	unsigned long eadd_cnt;
 	unsigned long resident;
@@ -70,6 +75,8 @@ struct enclave {
 	LIST_ENTRY(enclave) state_entry[2];
 	LIST_ENTRY(enclave) hash_entry;
 };
+
+FILE *enclave_log;
 
 /*
  * Keep an old and a new  list of enclaves, and a hash table.
@@ -126,17 +133,43 @@ int stats_read(struct stats *stats)
 }
 
 
-int stats_report(struct stats *old, struct stats *new)
+void stats_report(struct stats *old, struct stats *new)
 {
-	printf("%u/%u enclaves/created %lu added %lu pagins %lu pageouts %u total_resident %u va %u free\n",
-	       new->enclaves_created - new->enclaves_released,
-	       new->enclaves_created,
-	       new->pages_added,
-	       new->pageins - old->pageins,
-	       new->pageouts - old->pageouts,
-	       new->enclave_pages,
-	       new->va_pages,
-	       new->free_pages);
+	char enclave_str[COLS];
+
+	snprintf(enclave_str, sizeof(enclave_str), "%u/%u",
+		 new->enclaves_created - new->enclaves_released,
+		 new->enclaves_created);
+	mvprintw(0, 0, "%15s enclaves/created", enclave_str);
+
+	snprintf(enclave_str, sizeof(enclave_str), "%uK/%uK/%uK",
+		 new->va_pages * 4,
+		 (new->enclave_pages - new->free_pages) * 4,
+		 new->enclave_pages * 4);
+	printw("  %30s va/used/tot mem", enclave_str);
+
+	mvprintw(1, 0, "%10luK pageins", new->pageins - old->pageins);
+	mvprintw(2, 0, "%10luK pageouts", new->pageouts - old->pageouts);
+	refresh();
+}
+
+char *pid_read_comm(pid_t pid)
+{
+	FILE *fp;
+	char filename[] = "/proc/1234567890/comm";
+	char comm[TS_COMM_LEN];
+	char *result;
+
+	snprintf(filename, sizeof(filename), "/proc/%d/comm", pid);
+	fp = fopen(filename, "ro");
+	if (!fp)
+		return NULL;
+
+	fgets(comm, sizeof(comm), fp);
+	fclose(fp);
+
+	result = strdup(comm);
+	return result;
 }
 
 int enclave_read(FILE *fp, struct enclave *enclave)
@@ -148,18 +181,39 @@ int enclave_read(FILE *fp, struct enclave *enclave)
 	if (r != 5)
 		return -1;
 
+	if (enclave_log) {
+		fprintf(enclave_log, "%d %u %lu %lu %lu\n",
+			enclave->pid, enclave->id,
+			enclave->size, enclave->eadd_cnt,
+			enclave->resident);
+		fflush(enclave_log);
+	}
+
 	return 0;
 }
 
-int enclave_update(struct enclave *o, struct enclave *n)
+int enclave_update(struct enclave *o, struct enclave *n, unsigned which)
 {
+	assert(o->id == n->id);
 	o->old_eadd_cnt = o->eadd_cnt;
 	o->old_resident = o->resident;
 	o->eadd_cnt = n->eadd_cnt;
 	o->resident = n->resident;
 }
 
-struct enclaves *enclave_create(size_t n)
+int enclave_delete(struct enclaves *enclaves, struct enclave *e,
+		    unsigned which)
+{
+	free(e);
+}
+
+size_t hash_func(struct enclaves *e, unsigned int id)
+{
+	// Ugh!
+	return id % e->hash_table_size;
+}
+
+struct enclaves *enclaves_create(size_t n)
 {
 	struct enclaves *e = calloc(sizeof(struct enclaves), 1);
 	if (!e)
@@ -178,20 +232,15 @@ struct enclaves *enclave_create(size_t n)
 	LIST_INIT(&e->state_list[1]);
 }
 
-size_t hash_func(struct enclaves *e, unsigned int id)
-{
-	// Ugh!
-	return id % e->hash_table_size;
-}
-
 struct enclave *enclaves_find(struct enclaves *enclaves, unsigned int id)
 {
 	size_t bucket = hash_func(enclaves, id);
 	struct enclave *e = LIST_FIRST(&enclaves->hash_table[bucket]);
 
 	while (e) {
-		if (e->id)
+		if (e->id == id)
 			return e;
+		e = LIST_NEXT(e, hash_entry);
 	}
 	return NULL;
 }
@@ -202,70 +251,119 @@ int enclaves_insert(struct enclaves *enclaves, struct enclave *e)
 	LIST_INSERT_HEAD(&enclaves->hash_table[bucket], e, hash_entry);
 }
 
-int enclaves_delete(struct enclaves *enclaves, struct enclave *e)
+void enclaves_check_list(struct enclaves *enclaves)
 {
-	LIST_REMOVE(e, hash_entry);
+	struct enclave *e;
+	int count = 0;
+
+	LIST_FOREACH(e, &enclaves->state_list[enclaves->state],
+		     state_entry[enclaves->state]) {
+		assert(count++ <= enclaves->count);
+	}
 }
 
-int enclaves_read(struct enclaves *enclaves)
+void enclaves_read(struct enclaves *enclaves)
 {
 	FILE *fp;
 
-	// We track old and new entries, and have to swap which list
-	// represents new;
-	enclaves->state = !enclaves->state;
+	/*
+	 * We track old and new entries, and have to swap which list
+	 * represents new.
+	 */
 
-	if (!(fp = fopen(SGX_ENCLAVES, "ro")))
-		return -1;
+	unsigned old_state = enclaves->state;
+	unsigned new_state = !enclaves->state;
+
+	enclaves->state = new_state;
+	assert(enclaves->state_list[new_state].lh_first == NULL);
+
+	if (!(fp = fopen(SGX_ENCLAVES, "ro"))) {
+		fprintf(stderr, "Couldn't open %s\n", SGX_ENCLAVES);
+		exit(-1);
+	}
 
 	struct enclave enclave;
 	struct enclave *e;
+
+	enclaves->count = 0;
 	while (!enclave_read(fp, &enclave)) {
 		enclaves->count++;
 
 		if (e = enclaves_find(enclaves, enclave.id)) {
-			enclave_update(e, &enclave);
+			enclave_update(e, &enclave, old_state);
+			/*
+			 * Since this enclave was in the hash table,
+			 * it was pre-existing, and should be removed
+			 * from the list of old enclaves.
+			 */
+			LIST_REMOVE(e, state_entry[old_state]);
+			enclaves_check_list(enclaves);
 		} else {
 			e = malloc(sizeof(struct enclave));
 			if (!e)
-				return 0;
+				continue;  /* Skip it? */
+
 			*e = enclave;
+			e->old_eadd_cnt = e->old_resident = 0;
+			e->comm = pid_read_comm(e->pid);
+
 			enclaves_insert(enclaves, e);
+			enclaves_check_list(enclaves);
 		}
-		LIST_INSERT_HEAD(&enclaves->state_list[enclaves->state],
-				 e, state_entry[enclaves->state]);
+		/* Insert everybody in the list of current enclaves */
+		LIST_INSERT_HEAD(&enclaves->state_list[new_state],
+				 e, state_entry[new_state]);
+		enclaves_check_list(enclaves);
 	}
 	fclose(fp);
 
 	int r = clock_gettime(CLOCK_MONOTONIC, &enclaves->readtime);
-	if (r)
-		return r;
-
-	/* Iterate over the table of enclaves and remove old ones. */
-	while (!LIST_EMPTY(&enclaves->state_list[!enclaves->state])) {
-		e = LIST_FIRST(&enclaves->state_list[!enclaves->state]);
-		LIST_REMOVE(e, state_entry[!enclaves->state]);
-		LIST_REMOVE(e, hash_entry);
+	if (r) {
+		fprintf(stderr, "Clock failed to read!\n");
+		exit(-1);
 	}
 
-	return 0;
+	/* Iterate over the table of old enclaves and remove each one. */
+	while (!LIST_EMPTY(&enclaves->state_list[old_state])) {
+		e = LIST_FIRST(&enclaves->state_list[old_state]);
+		LIST_REMOVE(e, state_entry[old_state]);
+		LIST_REMOVE(e, hash_entry);
+		enclave_delete(enclaves, e, old_state);
+	}
+	assert(enclaves->state_list[old_state].lh_first == NULL);
 }
 
-int enclaves_report(struct enclaves *enclaves)
+void enclaves_report(struct enclaves *enclaves)
 {
 	struct enclave *e;
+	unsigned line = 4;
+	static unsigned last_lines;
+
+	mvprintw(line++, 0, "%5s %10s %11s %11s %11s %10s",
+		 "PID", "ID", "Size", "EADDed", "Resident", "Command");
 	LIST_FOREACH(e, &enclaves->state_list[enclaves->state],
 		     state_entry[enclaves->state]) {
-		printf("%d %u size %luK eadded %luK resident %luK\n",
-		       e->pid, e->id, e->size / 1024, e->eadd_cnt * 4,
-		       e->resident * 4);
+		assert(line <= enclaves->count + 4 /* initial count */);
+		if (line < LINES)
+			mvprintw(line++, 0, "%5d %10u %10luK %10luK %10luK %s",
+				 e->pid, e->id, e->size / 1024,
+				 e->eadd_cnt * 4, e->resident * 4,
+				 e->comm ? e->comm : "");
 	}
+
+	/* Clear any leftover lines from the last display */
+	while (last_lines > line) {
+		move(line++, 0);
+		clrtoeol();
+	}
+	refresh();
+	last_lines = line;
 }
 
 int main(int argc, char **argv)
 {
 	struct stats old, new;
-	struct enclaves *enclaves = enclave_create(100);
+	struct enclaves *enclaves = enclaves_create(101);
 
 	if (!enclaves)
 		exit(-1);
@@ -273,16 +371,29 @@ int main(int argc, char **argv)
 	if (stats_read(&new))
 		exit(-1);
 
+	/* Fire up curses. */
+
+	initscr();
+	cbreak();
+	noecho();
+	curs_set(0);
+	clear();
+
+	enclave_log = fopen("/tmp/enclave_log", "w");
+
 	struct timespec now = new.readtime;
 	while (1) {
 		old = new;
 		now.tv_sec++;
 		if (sleep_til(&now))
 			exit(-1);
-		stats_read(&new);
+		if (stats_read(&new))
+			exit(-1);
 		enclaves_read(enclaves);
 		stats_report(&old, &new);
 		enclaves_report(enclaves);
+		/* Accept input, for instance for a redraw? */
 	}
+	endwin();  // Not reached
 	return 0;
 }
