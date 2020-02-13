fb5fe29982d33b57d6d41e2bf24b3e34801c53da
Vishwas Pai <vishwas.pai@fortanix.com>
2020-02-13 10:22:55 +0530

    Changed default installation path to /usr/local/bin.

--- maintainer.sh
+++ maintainer.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
-set -x
-
 aclocal # Set up an m4 environment
 autoconf # Generate configure from configure.ac
 automake --add-missing # Generate Makefile.in from Makefile.am
 
 #Test configure & build
-./configure --prefix=$PWD CC=clang
+./configure CC=clang
 make
-make install
+
+#install sgxtop / sgxstat link
+sudo make install
+echo "+-----------------------------------------------------------------------------------------------------------------+"
+echo "|  sgxtop/sgxstat installed in /usr/local/bin/ (default) or at the path supplied in --prefix option of configure. |"
+echo "|  Check if the installed path is in your PATH environment variable.                                              |"
+echo "|  $ sudo make uninstall  - to remove installed binaries.                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------+"
+
+#uninstall will remove sgxtop/sgxstat from --prefix path (default: /usr/local/bin/)
+# -- uncomment if uninstall is needed.
+#sudo make uninstall
 
 #Cleanup
 #files created by auto* tools (m4 files, configure etc) will remain.
-make clean
-make uninstall
+# -- uncomment if cleanup is needed.
+#make clean
