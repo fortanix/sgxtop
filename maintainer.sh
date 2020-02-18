fe2a57353dd20464032529db87817218aea0a55d
Vishwas Pai <vishwas.pai@fortanix.com>
2020-02-18 09:53:06 +0530

    Removed clang & commented install

--- maintainer.sh
+++ maintainer.sh
@@ -4,22 +4,26 @@ autoconf # Generate configure from configure.ac
 automake --add-missing # Generate Makefile.in from Makefile.am
 
 #Test configure & build
-./configure CC=clang
+./configure
 make
 
 #install sgxtop / sgxstat link
-sudo make install
-echo "+-----------------------------------------------------------------------------------------------------------------+"
-echo "|  sgxtop/sgxstat installed in /usr/local/bin/ (default) or at the path supplied in --prefix option of configure. |"
-echo "|  Check if the installed path is in your PATH environment variable.                                              |"
-echo "|  $ sudo make uninstall  - to remove installed binaries.                                                         |"
-echo "+-----------------------------------------------------------------------------------------------------------------+"
+#
+# -- uncomment if install is needed.
+#sudo make install
+#echo "+-----------------------------------------------------------------------------------------------------------------+"
+#echo "|  sgxtop/sgxstat installed in /usr/local/bin/ (default) or at the path supplied in --prefix option of configure. |"
+#echo "|  Check if the installed path is in your PATH environment variable.                                              |"
+#echo "|  $ sudo make uninstall  - to remove installed binaries.                                                         |"
+#echo "+-----------------------------------------------------------------------------------------------------------------+"
 
 #uninstall will remove sgxtop/sgxstat from --prefix path (default: /usr/local/bin/)
+#
 # -- uncomment if uninstall is needed.
 #sudo make uninstall
 
 #Cleanup
 #files created by auto* tools (m4 files, configure etc) will remain.
+#
 # -- uncomment if cleanup is needed.
 #make clean
