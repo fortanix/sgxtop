a04445e4a2ce47d9871e858e5f94fd874f85ffb9
Vishwas Pai <vishwas.pai@fortanix.com>
2020-02-26 07:36:52 +0530

    Removed "sudo" from scripts.

--- maintainer.sh
+++ maintainer.sh
@@ -10,17 +10,17 @@ make
 #install sgxtop / sgxstat link
 #
 # -- uncomment if install is needed.
-#sudo make install
+#make install
 #echo "+-----------------------------------------------------------------------------------------------------------------+"
 #echo "|  sgxtop/sgxstat installed in /usr/local/bin/ (default) or at the path supplied in --prefix option of configure. |"
 #echo "|  Check if the installed path is in your PATH environment variable.                                              |"
-#echo "|  $ sudo make uninstall  - to remove installed binaries.                                                         |"
+#echo "|  $ make uninstall  - to remove installed binaries.                                                              |"
 #echo "+-----------------------------------------------------------------------------------------------------------------+"
 
 #uninstall will remove sgxtop/sgxstat from --prefix path (default: /usr/local/bin/)
 #
 # -- uncomment if uninstall is needed.
-#sudo make uninstall
+#make uninstall
 
 #Cleanup
 #files created by auto* tools (m4 files, configure etc) will remain.
